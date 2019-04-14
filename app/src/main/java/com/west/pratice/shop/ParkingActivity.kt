7ca20a1ed4a8dc466fb346cbfe3c73b2dbb620aa
ty61271 <ty61271@yahoo.com.tw>
2019-04-14 19:24:01 +0800

    ch7-2 (Learn) Anko AsyncTask、Toast、Log、Alert

--- app/src/main/java/com/west/pratice/shop/ParkingActivity.kt
+++ app/src/main/java/com/west/pratice/shop/ParkingActivity.kt
@@ -6,9 +6,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_parking.*
+import org.jetbrains.anko.*
 import java.net.URL
 
-class ParkingActivity : AppCompatActivity() {
+class ParkingActivity : AppCompatActivity(), AnkoLogger {
 
     private val TAG = ParkingActivity::class.java.simpleName
 
@@ -17,7 +18,22 @@ class ParkingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_parking)
         val parking =
             "https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f"
-        ParkingTask().execute(parking)
+
+//        Anko
+        doAsync {
+            val url = URL(parking)
+            val json = url.readText()
+            info(json)
+            uiThread {
+                toast("Got it")
+                info.text = json
+                alert("Got it","ALERT"){
+                    okButton {  }
+                }.show()
+            }
+        }
+
+//        ParkingTask().execute(parking)
     }
 
     inner class ParkingTask : AsyncTask<String, Void, String>() {
