f2176417adf0b22457bae3c37b43b4fb415c5d9e
ty61271 <ty61271@yahoo.com.tw>
2019-04-14 18:52:38 +0800

    ch7-1 (Learn) 網路連線 AsyncTask

---
+++ app/src/main/java/com/west/pratice/shop/ParkingActivity.kt
@@ -0,0 +1,37 @@
+package com.west.pratice.shop
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_parking.*
+import java.net.URL
+
+class ParkingActivity : AppCompatActivity() {
+
+    private val TAG = ParkingActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parking)
+        val parking =
+            "https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f"
+        ParkingTask().execute(parking)
+    }
+
+    inner class ParkingTask : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            val url = URL(params[0])
+            val json = url.readText()
+            Log.d(TAG, "doInBackground: $json")
+            return json
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            Toast.makeText(this@ParkingActivity, "Got it", Toast.LENGTH_LONG).show()
+            info.text = result
+        }
+    }
+}
